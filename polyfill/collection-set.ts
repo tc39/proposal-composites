1aeebc1318350aef09dd8fb21c0e75d9b93b31c2
Ashley Claymore <aclaymore@bloomberg.net>
2025-04-07 11:11:55 +0100

    polyfill: set methods

--- polyfill/collection-set.ts
+++ polyfill/collection-set.ts
@@ -1,4 +1,15 @@
-import { _Map, apply } from "./internal/originals.ts";
+import {
+    _Map,
+    mapSize,
+    apply,
+    min as toNumber,
+    isNaN,
+    abs,
+    floor,
+    NEGATIVE_INFINITY,
+    POSITIVE_INFINITY,
+    iterator,
+} from "./internal/originals.ts";
 import { mapPrototypeMethods as mapMethods } from "./collection-map.ts";
 
 /**
@@ -15,6 +26,8 @@ export class Set<T> implements globalThis.Set<T> {
         }
     }
 
+    #requireInternalSlot() {}
+
     add(value: T): this {
         apply(mapMethods.set, this.#map, [value, 1]);
         return this;
@@ -28,18 +41,26 @@ export class Set<T> implements globalThis.Set<T> {
         return apply(mapMethods.delete, this.#map, [value]);
     }
 
-    forEach(callbackfn: (value: T, value2: T, set: this) => void, thisArg?: any): void {
+    forEach(callbackfn: (value: T, value2: T, set: globalThis.Set<T>) => void, thisArg?: any): void {
         this.#map.forEach((_, key) => {
             callbackfn.call(thisArg, key, key, this);
         });
     }
 
-    has(value: T): boolean {
+    #has(value: T): boolean {
         return apply(mapMethods.has, this.#map, [value]);
     }
 
+    has(value: T): boolean {
+        return this.#has(value);
+    }
+
+    #size(): number {
+        return apply(mapSize, this.#map, []);
+    }
+
     get size(): number {
-        return this.#map.size;
+        return this.#size();
     }
 
     *entries(): SetIterator<[T, T]> {
@@ -56,6 +77,126 @@ export class Set<T> implements globalThis.Set<T> {
         return this.#map.keys();
     }
 
+    union<U>(other: ReadonlySetLike<U>): globalThis.Set<T | U> {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+        const result = new Set<T | U>();
+        for (const value of this) {
+            apply(mapMethods.set, result.#map, [value, 1]);
+        }
+        for (const value of otherSet.keys()) {
+            apply(mapMethods.set, result.#map, [value, 1]);
+        }
+        return result;
+    }
+
+    intersection<U>(other: ReadonlySetLike<U>): globalThis.Set<T & U> {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+        const result = new Set<T & U>();
+
+        if (this.#size() <= otherSet.size) {
+            for (const value of this) {
+                if (otherSet.has(value)) {
+                    apply(mapMethods.set, result.#map, [value, 1]);
+                }
+            }
+        } else {
+            for (const value of otherSet.keys()) {
+                if (this.#has(value as any)) {
+                    apply(mapMethods.set, result.#map, [value, 1]);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    difference<U>(other: ReadonlySetLike<U>): globalThis.Set<T> {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+        const result = new Set<T>();
+        for (const value of this) {
+            apply(mapMethods.set, result.#map, [value, 1]);
+        }
+
+        if (result.#size() <= otherSet.size) {
+            for (const value of result) {
+                if (otherSet.has(value)) {
+                    apply(mapMethods.delete, result.#map, [value]);
+                }
+            }
+        } else {
+            for (const value of otherSet.keys()) {
+                apply(mapMethods.delete, result.#map, [value]);
+            }
+        }
+
+        return result;
+    }
+
+    symmetricDifference<U>(other: ReadonlySetLike<U>): globalThis.Set<T | U> {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+        const result = new Set<T | U>();
+        for (const value of this) {
+            if (!otherSet.has(value)) {
+                apply(mapMethods.set, result.#map, [value, 1]);
+            }
+        }
+        for (const value of otherSet.keys()) {
+            if (!this.#has(value as any)) {
+                apply(mapMethods.set, result.#map, [value, 1]);
+            }
+        }
+        return result;
+    }
+
+    isSubsetOf(other: ReadonlySetLike<unknown>): boolean {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+        if (this.#size() > otherSet.size) return false;
+        for (const value of this) {
+            if (!otherSet.has(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isSupersetOf(other: ReadonlySetLike<unknown>): boolean {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+        if (this.#size() < otherSet.size) return false;
+        for (const value of otherSet.keys()) {
+            if (!this.#has(value as any)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isDisjointFrom(other: ReadonlySetLike<unknown>): boolean {
+        this.#requireInternalSlot();
+        const otherSet = getSetRecord(other);
+
+        if (this.#size() <= otherSet.size) {
+            for (const value of this) {
+                if (otherSet.has(value)) {
+                    return false;
+                }
+            }
+        } else {
+            for (const value of otherSet.keys()) {
+                if (this.#has(value as any)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     [Symbol.iterator](): SetIterator<T> {
         return this.#map.keys();
     }
@@ -63,4 +204,65 @@ export class Set<T> implements globalThis.Set<T> {
     get [Symbol.toStringTag](): string {
         return "Set";
     }
+}
+
+function getSetRecord(other: ReadonlySetLike<unknown>) {
+    const size = toNumber(other.size);
+    if (isNaN(size)) {
+        throw new TypeError("invalid size");
+    }
+    const intSize = toIntegerOrInfinity(size);
+    if (intSize < 0) {
+        throw new RangeError("invalid size");
+    }
+    const has = other.has;
+    if (typeof has !== "function") {
+        throw new TypeError("invalid has");
+    }
+    const keys = other.keys;
+    if (typeof keys !== "function") {
+        throw new TypeError("invalid keys");
+    }
+    return {
+        obj: other,
+        size: intSize,
+        has(v: any): boolean {
+            return Boolean(apply(has, other, [v]));
+        },
+        keys(): Iterable<any> {
+            const it = apply(keys, other, []);
+            if (it === null || typeof it !== "object") {
+                throw new TypeError("invalid keys");
+            }
+            const next = it.next;
+            return {
+                [iterator as typeof Symbol.iterator]() {
+                    return {
+                        __proto__: null,
+                        next() {
+                            return apply(next, it, []);
+                        },
+                    };
+                },
+            };
+        },
+    };
+}
+
+function toIntegerOrInfinity(arg: unknown): number {
+    const n = toNumber(arg as number);
+    if (isNaN(n) || n === 0) {
+        return 0;
+    }
+    if (n === POSITIVE_INFINITY) {
+        return POSITIVE_INFINITY;
+    }
+    if (n === NEGATIVE_INFINITY) {
+        return NEGATIVE_INFINITY;
+    }
+    let i = floor(abs(n));
+    if (n < 0) {
+        i = -i;
+    }
+    return i;
 }
--- polyfill/internal/originals.ts
+++ polyfill/internal/originals.ts
@@ -1,8 +1,9 @@
-export const { isNaN, NaN } = Number;
-export const { apply, ownKeys, defineProperty, preventExtensions } = Reflect;
+export const { isNaN, NaN, POSITIVE_INFINITY, NEGATIVE_INFINITY } = Number;
+export const { abs, floor, min } = Math;
+export const { apply, ownKeys, defineProperty, preventExtensions, getOwnPropertyDescriptor } = Reflect;
 export const { is, freeze } = Object;
 export const { sort, splice, includes, indexOf, lastIndexOf } = Array.prototype;
-export const { keyFor } = Symbol;
+export const { keyFor, iterator } = Symbol;
 export const { localeCompare } = String.prototype;
 export const _Map = Map;
 export const {
@@ -16,6 +17,7 @@ export const {
     entries: mapEntries,
     forEach: mapForEach,
 } = Map.prototype;
+export const mapSize = getOwnPropertyDescriptor(Map.prototype, "size")!.get!;
 export const _Set = Set;
 export const { has: setHas, add: setAdd } = Set.prototype;
 export const _WeakSet = WeakSet;
